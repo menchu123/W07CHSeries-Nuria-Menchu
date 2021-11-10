b005a93e9a7def0dc46b34c690be0c2dd8d19dcd
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-10 20:59:46 +0100

    Tested updatePlatforms function successfully

--- server/controller/platformsController.js
+++ server/controller/platformsController.js
@@ -19,13 +19,19 @@ const createPlatforms = async (req, res, next) => {
 const updatePlatforms = async (req, res, next) => {
   try {
     const { _id } = req.body;
-    const updatedPlatform = await Platform.findById(_id, req.body, {
+    const updatedPlatform = await Platform.findByIdAndUpdate(_id, req.body, {
       new: true,
     });
-    res.json(updatedPlatform);
+    if (updatedPlatform) {
+      res.json(updatedPlatform);
+    } else {
+      const error = new Error("Platform not found");
+      error.code = 404;
+      next(error);
+    }
   } catch (error) {
     error.code = 400;
-    error.message = "Unauthorized";
+    error.message = "Update failed";
     next(error);
   }
 };
