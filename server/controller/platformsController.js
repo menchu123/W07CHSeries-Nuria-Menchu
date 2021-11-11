afdb0a3e9377bc3688f93f6a5ee9395b3bce1f78
Menchu MV <mcmvillalobos@gmail.com>
2021-11-11 19:49:27 +0100

    Added admin condition to restricted platfrom routes and fixed corresponding tests

--- server/controller/platformsController.js
+++ server/controller/platformsController.js
@@ -6,43 +6,64 @@ const getPlatforms = async (req, res) => {
 };
 
 const createPlatforms = async (req, res, next) => {
-  const newPlatform = await Platform.create(req.body);
-  res.json(newPlatform);
+  const { admin } = req.userInfo;
+  if (!admin) {
+    const error = new Error("You wish");
+    error.code = 401;
+    next(error);
+  } else {
+    const newPlatform = await Platform.create(req.body);
+    res.json(newPlatform);
+  }
 };
 
 const updatePlatforms = async (req, res, next) => {
-  const { idPlatform } = req.params;
-  const updatedPlatform = await Platform.findByIdAndUpdate(
-    idPlatform,
-    req.body,
-    {
-      new: true,
-    }
-  );
-  if (updatedPlatform) {
-    res.json(updatedPlatform);
-  } else {
-    const error = new Error("Platform not found");
-    error.code = 404;
+  const { admin } = req.userInfo;
+  if (!admin) {
+    const error = new Error("You wish");
+    error.code = 401;
     next(error);
+  } else {
+    const { idPlatform } = req.params;
+    const updatedPlatform = await Platform.findByIdAndUpdate(
+      idPlatform,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    if (updatedPlatform) {
+      res.json(updatedPlatform);
+    } else {
+      const error = new Error("Platform not found");
+      error.code = 404;
+      next(error);
+    }
   }
 };
 
 const deletePlatforms = async (req, res, next) => {
-  try {
-    const { idPlatform } = req.params;
-    const deletedPlatform = await Platform.findByIdAndDelete(idPlatform);
-    if (deletedPlatform) {
-      res.json(deletePlatforms);
-    } else {
-      const error = new Error("Not found");
-      error.code = 404;
+  const { admin } = req.userInfo;
+  if (!admin) {
+    const error = new Error("You wish");
+    error.code = 401;
+    next(error);
+  } else {
+    try {
+      const { idPlatform } = req.params;
+      const deletedPlatform = await Platform.findByIdAndDelete(idPlatform);
+      if (deletedPlatform) {
+        res.json(deletePlatforms);
+      } else {
+        const error = new Error("Not found");
+        error.code = 404;
+        next(error);
+      }
+    } catch (error) {
+      error.code = 400;
+      error.message = "Cannot delete sorry :(";
       next(error);
     }
-  } catch (error) {
-    error.code = 400;
-    error.message = "Cannot delete sorry :(";
-    next(error);
   }
 };
 
--- server/controller/platformsController.test.js
+++ server/controller/platformsController.test.js
@@ -41,6 +41,9 @@ describe("Given a createPlatforms function", () => {
       };
 
       const req = {
+        userInfo: {
+          admin: true,
+        },
         body: platform,
       };
 
@@ -58,6 +61,38 @@ describe("Given a createPlatforms function", () => {
       expect(res.json).toHaveBeenCalledWith(platform);
     });
   });
+  describe("When it receives an object res, an object req with a body but the user is not an admin", () => {
+    test("Then it should invoke next with a 'You wish' error", async () => {
+      const platform = {
+        name: "Ororo",
+        price: "6",
+      };
+
+      const req = {
+        userInfo: {
+          admin: false,
+        },
+        body: platform,
+      };
+
+      const res = {};
+
+      const expectedError = {
+        code: 401,
+        message: "You wish",
+      };
+
+      const next = jest.fn();
+
+      await createPlatforms(req, res, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        expectedError.message
+      );
+      expect(next.mock.calls[0][0]).toHaveProperty("code", expectedError.code);
+    });
+  });
 });
 
 describe("Given a updatePlatforms function", () => {
@@ -66,11 +101,16 @@ describe("Given a updatePlatforms function", () => {
       const newPlatform = {
         name: "Ororo",
         price: "8",
-        id: 1,
       };
 
       const req = {
-        params: newPlatform,
+        userInfo: {
+          admin: true,
+        },
+        params: {
+          id: 1,
+        },
+        body: newPlatform,
       };
 
       Platform.findByIdAndUpdate = jest.fn().mockResolvedValue(newPlatform);
@@ -88,11 +128,50 @@ describe("Given a updatePlatforms function", () => {
     });
   });
 
+  describe("When it receives an object res, an object req with a body, but the user is not admin", () => {
+    test("Then it should invoke next with a 'You wish' error", async () => {
+      const newPlatform = {
+        name: "Ororo",
+        price: "8",
+      };
+
+      const req = {
+        userInfo: {
+          admin: false,
+        },
+        params: {
+          id: 1,
+        },
+        body: newPlatform,
+      };
+
+      const res = {};
+
+      const expectedError = {
+        code: 401,
+        message: "You wish",
+      };
+
+      const next = jest.fn();
+
+      await updatePlatforms(req, res, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        expectedError.message
+      );
+      expect(next.mock.calls[0][0]).toHaveProperty("code", expectedError.code);
+    });
+  });
+
   describe("When it receives a non existent platform", () => {
     test("Then it should invoke a next function with an error status 404 ", async () => {
       Platform.findByIdAndUpdate = jest.fn().mockResolvedValue(null);
 
       const req = {
+        userInfo: {
+          admin: true,
+        },
         params: {
           id: "1234",
         },
@@ -130,6 +209,9 @@ describe("Given a deletePlatforms function", () => {
       };
 
       const req = {
+        userInfo: {
+          admin: true,
+        },
         params: { idPlatform },
       };
 
@@ -147,6 +229,42 @@ describe("Given a deletePlatforms function", () => {
     });
   });
 
+  describe("When it receives a request with an id of a platform, an object res with a body but the user is not admin", () => {
+    test("Then it should invoke next with a 'You wish' error ", async () => {
+      const idPlatform = "1234";
+
+      const platform = {
+        name: "Ororo",
+        price: "8",
+        id: idPlatform,
+      };
+
+      const req = {
+        userInfo: {
+          admin: false,
+        },
+        params: { idPlatform },
+      };
+
+      const res = {};
+
+      const next = jest.fn();
+
+      const expectedError = {
+        code: 401,
+        message: "You wish",
+      };
+
+      await deletePlatforms(req, res, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        expectedError.message
+      );
+      expect(next.mock.calls[0][0]).toHaveProperty("code", expectedError.code);
+    });
+  });
+
   describe("When it receives a non existent platform and findByIdAndDelete is rejected", () => {
     test("Then it should invoke a next function with an error status 404 ", async () => {
       const error = {};
@@ -154,6 +272,9 @@ describe("Given a deletePlatforms function", () => {
       Platform.findByIdAndDelete = jest.fn().mockRejectedValue(error);
 
       const req = {
+        userInfo: {
+          admin: true,
+        },
         params: { idPlatform: 0 },
       };
       const res = {
