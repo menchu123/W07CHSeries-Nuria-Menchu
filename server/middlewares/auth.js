0b0fb5728545573af7633aae4511499465bb3ba5
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-11 18:52:12 +0100

    Refactorized auth middleware and tested

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,23 +3,24 @@ const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
   const authHeader = req.header("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authorized");
+    const error = new Error("Not authorized sorry");
     error.code = 401;
     next(error);
   } else {
-    const [, token] = authHeader.split(" ");
+    const token = authHeader.split(" ")[1];
 
     if (!token) {
-      const error = new Error("Token is missing");
+      const error = new Error("Token is missing...");
       error.code = 401;
       next(error);
     } else {
       try {
-        const user = await jwt.verify(token, process.env.SECRET);
+        const user = jwt.verify(token, process.env.SECRET);
         req.userId = user.id;
         next();
       } catch (error) {
         error.message = "Token no valid";
+        error.code = 401;
         next(error);
       }
     }
---
+++ server/middlewares/auth.test.js
@@ -0,0 +1,54 @@
+const auth = require("./auth");
+
+describe("Given an auth middleware", () => {
+  describe("When it gets a request with an incorrect Authorization header", () => {
+    test("Then it should send an error with a message 'Not authorized sorry' and status 401", () => {
+      const req = {
+        header: jest.fn(),
+      };
+
+      const res = {};
+
+      const next = jest.fn();
+
+      const expectedError = new Error("Not authorized sorry");
+
+      auth(req, res, next);
+      expect(next).toBeCalledWith(expectedError);
+    });
+  });
+  describe("When it gets a request with a Authorization header but without a token", () => {
+    test("Then it should send an error with a message 'Token is missing' and status 401", () => {
+      const authHeader = "nunu";
+
+      const req = {
+        header: jest.fn().mockReturnValue(authHeader),
+      };
+
+      const res = {};
+      const next = jest.fn();
+      const expectedError = new Error("Token is missing...");
+
+      auth(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When it gets a request with a Authorization header but with an incorrect token", () => {
+    test("Then it should send an error with a message 'Token no valid' and status 401", async () => {
+      const authHeader = "nunu";
+
+      const req = {
+        header: jest.fn().mockReturnValue(authHeader),
+      };
+
+      const res = {};
+      const next = jest.fn();
+      const errorSent = new Error("Token no valid");
+
+      await auth(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(errorSent);
+    });
+  });
+});
