731174ab0fe2d4ea91a0468713d34dcc2728b689
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-11 19:26:27 +0100

    Tried to test token invalid on auth unsuccessfully

--- server/middlewares/auth.test.js
+++ server/middlewares/auth.test.js
@@ -1,4 +1,7 @@
 const auth = require("./auth");
+const jwt = require("jsonwebtoken");
+
+jest.mock("jsonwebtoken");
 
 describe("Given an auth middleware", () => {
   describe("When it gets a request with an incorrect Authorization header", () => {
@@ -36,18 +39,23 @@ describe("Given an auth middleware", () => {
   });
   describe("When it gets a request with a Authorization header but with an incorrect token", () => {
     test("Then it should send an error with a message 'Token no valid' and status 401", async () => {
-      const authHeader = "nunu";
-
       const req = {
-        header: jest.fn().mockReturnValue(authHeader),
+        json: jest.fn(),
+        header: jest.fn().mockReturnValue("Bearer token"),
       };
 
-      const res = {};
       const next = jest.fn();
       const errorSent = new Error("Token no valid");
+      errorSent.code = 401;
 
+      const error = new Error();
+
+      const res = {};
+
+      jwt.verify = jest.fn().mockRejectedValue(error);
       await auth(req, res, next);
 
+      expect(next).toHaveBeenCalled();
       expect(next).toHaveBeenCalledWith(errorSent);
     });
   });
